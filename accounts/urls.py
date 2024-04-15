9264f7d49098bc55b0fde7628299bc3356990240
ERFAN <erfansafarzad7@gmail.com>
2024-04-15 15:55:31 +0330

    orderAdmin+cartAdmin-updated+someChanges

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,6 @@ from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView)
 
 
-app_name = 'auth'
-
 urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='login'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
